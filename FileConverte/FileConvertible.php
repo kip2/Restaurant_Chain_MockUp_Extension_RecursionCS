1aac993f37cadf3fe1df39137e6a6ac0cea30e52
kip2 <tqa1no4@gmail.com>
2023-10-05 21:54:23 +0900

    add: FileConvertibleのメソッド定義を追加

--- FileConverte/FileConvertible.php
+++ FileConverte/FileConvertible.php
@@ -1,5 +1,9 @@
 <?php
 
 interface FileConvertible {
+    public function toString():string;
+    public function toHTML():string;
+    public function toMarkdown():string;
+    public function toArray():array;
 
 }
