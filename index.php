5128689147855fbfadd8bced307aa1369de75c3e
kip2 <tqa1no4@gmail.com>
2023-10-05 23:12:11 +0900

    add: viewの型枠を追加

--- index.php
+++ index.php
@@ -16,6 +16,24 @@
     <body>
         <!-- todo: 中央に寄せる必要がある -->
         <h1>Restaurant Chain List</h1>
+        <div>
+            <!-- todo: borderで囲う -->
+            <h2>Restaurant Chain HOGEHOGE, FUGAFUGA</h2>
+            <div>
+                <p>restaurant name and folding infomation </p>
+                <div>
+                    <p>company information</p>
+                    <div>
+                        <p>employees:</p>
+                        <div>
+                            <p>employee1</p>
+                            <p>employee2</p>
+                            <p>employee3</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
 
