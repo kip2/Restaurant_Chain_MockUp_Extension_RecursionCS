053b4a81fcfecdc6c2b47ffa5452a3ecf2652de2
kip2 <tqa1no4@gmail.com>
2023-10-05 21:52:49 +0900

    add: RestaurantChainのメンバ変数定義を追加

--- Companyies/Restaurants/RestaurantChain.php
+++ Companyies/Restaurants/RestaurantChain.php
@@ -4,6 +4,13 @@
 
 class RestaurantChain extends Company {
 
+    private int $chainId;
+    // todo: RestaurantLocationの配列として宣言する必要がある
+    private array $restaurantLocations;
+    private string $cuisineType;
+    private int $numberOfLocations;
+    private string $parentCompany;
+
     // todo: 親のコンストラクタに明示的に渡すこと
 
 }
