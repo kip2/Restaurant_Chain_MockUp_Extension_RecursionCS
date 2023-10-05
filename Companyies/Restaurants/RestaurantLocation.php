aea270794ebc9a5434f16b3b08152e381795a7f5
kip2 <tqa1no4@gmail.com>
2023-10-05 21:51:10 +0900

    add: RestaurantLocationのメンバ変数定義を追加

--- Companyies/Restaurants/RestaurantLocation.php
+++ Companyies/Restaurants/RestaurantLocation.php
@@ -2,4 +2,14 @@
 
 class RestaurantLocation {
 
+    private string $name;
+    private string $address;
+    private string $city;
+    private string $state;
+    private string $zipCode;
+    // todo: Employeeクラスの配列である宣言に修正する必要ある
+    private array $employees;
+    private bool $isOpen;
+    private bool $hasDriveThru;
+
 }
