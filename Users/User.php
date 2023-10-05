f9d788b311805d3c6ece6010671621744a0d1591
kip2 <tqa1no4@gmail.com>
2023-10-05 21:45:38 +0900

    add: user.phpのメソッドとメンバ変数の定義

--- Users/User.php
+++ Users/User.php
@@ -1,5 +1,39 @@
 <?php
 
 class User{
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private string $email;
+    private string $hashedPassword;
+    private string $phoneNumber;
+    private string $address;
+    // todo: DateTimeの依存関係解消
+    private DateTime $birthDate;
+    // todo: DateTimeの依存関係解消
+    private DateTIme $membershipExpirationDate;
+    private string $role;
+
+    // --------------------------------------------------
+    // method
+    // --------------------------------------------------
+
+    // todo: 各メソッドの動作を把握する
+    public function login(string $password) {
+
+    }
+
+    public function updateProfile(string $address, string $phoneNumber) {
+
+    }
+    public function renewMembership(DateTime $expirationDate) {
+
+    }
+    public function changePassword(string $newPassword) {
+
+    }
+    public function hasMembershipExpired() {
+
+    }
 
 }
