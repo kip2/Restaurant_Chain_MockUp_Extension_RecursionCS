682f64f2ed2a8bea6194b82fa797e5e37c29fd7f
kip2 <tqa1no4@gmail.com>
2023-10-05 22:39:56 +0900

    viewの枠組みを作成

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
 
-echo "Hello!";
+// todo: 各クラスの呼び出し
+// todo: viewの作成
+// todo: 
+
+// echo "Hello!";
+
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> Restaurant Chain Moclup </title>
+    </head>
+    <body>
+        <!-- todo: 中央に寄せる必要がある -->
+        <h1>Restaurant Chain List</h1>
+    </body>
+</html>
 
