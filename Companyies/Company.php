c4f0239e47d67b0a1f4b7e8069f95fee10f2005a
kip2 <tqa1no4@gmail.com>
2023-10-05 21:49:19 +0900

    add: companyのメンバ変数定義

--- Companyies/Company.php
+++ Companyies/Company.php
@@ -1,5 +1,16 @@
 <?php
 
 class Company {
+    private string $name;
+    private int $foudingYear;
+    private string $description;
+    private string $website;
+    private string $phone;
+    private string $industory;
+    private string $ceo;
+    private bool $isPubliclyTraded;
+    private string $country;
+    private string $founder;
+    private int $totalEmployees;
 
 }
