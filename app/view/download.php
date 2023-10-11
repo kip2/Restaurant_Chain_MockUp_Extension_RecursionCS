7fbef5dba87ee465bbb155aa34cc71025e706203
kip2 <tqa1no4@gmail.com>
2023-10-11 21:46:57 +0900

    fix: 変数名を変更

--- app/view/download.php
+++ app/view/download.php
@@ -7,6 +7,22 @@ $format = $_POST['format'] ?? 'html';
 $count = (int)$count;
 $users = RandomGenerator::users($count, $count);
 
+// validation
+// todo: typeは使ってないけどどこにある?...
+if (is_null($count) || is_null($format)) {
+    exit("Missing parameters.");
+}
+
+if (!is_numeric($count) || $count < 1 || $count > 100) {
+    exit("Invalid count. Must be a number between 1 and 100.");
+}
+
+$allowedFormats = ['josn', 'txt', 'html', 'md'];
+if (!in_array($format, $allowdTypes)) {
+    exit('Invalied type. Must be one of: ' . implode(', ' , $allowdFormats));
+}
+
+
 match ($format) {
     "markdown" => toMarkdown($users),
     "json" => toJson($users),
