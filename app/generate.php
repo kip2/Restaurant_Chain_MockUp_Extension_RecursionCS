e3f050891409d85ee0a6ca1cbd30fe147c996719
kip2 <tqa1no4@gmail.com>
2023-10-11 21:01:21 +0900

    add: generate & リダイレクトで遷移するようにした

---
+++ app/generate.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Generate Users</title>
+</head>
+<body>
+    <form action="src/download.php" method="post">
+        <label for="count">Input Number of Users: </label>
+        <input type="number" id="count" name="count" min="1" max="100" value="5">
+
+        <label for="format">Select Download Format: </label>
+        <select name="format">
+            <option value="html">HTML</option>
+            <option value="markdown">Markdown</option>
+            <option value="json">JSON</option>
+            <option value="txt">Text</option>
+        </select>
+
+        <button type="submit">Generate</button>
+    </form>
+</body>
+</html>
