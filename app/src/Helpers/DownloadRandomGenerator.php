d7e6995750358165282b9481f5d2c2813bf55e4f
kip2 <tqa1no4@gmail.com>
2023-10-13 22:21:16 +0900

    add: download用randomgenerator作成

---
+++ app/src/Helpers/DownloadRandomGenerator.php
@@ -0,0 +1,337 @@
+<?php
+
+// namespace Helpers;
+
+require_once __DIR__ . '/../../src/Users/User.php';
+require_once __DIR__ . '/../../src/Users/Employees/Employee.php';
+require_once __DIR__ . '/../../src/Companies/Company.php';
+require_once __DIR__ . '/../../src/Companies/Restaurants/RestaurantLocation.php';
+require_once __DIR__ . '/../../src/Companies/Restaurants/RestaurantChain.php';
+
+require 'vendor/autoload.php';
+
+use Faker\Factory;
+
+$employee = RandomGenerator::employee();
+// echo $employee->introduction();
+
+class RandomGenerator {
+
+    // todo: 必ず最後にメソッドのコメントを直すこと
+
+    // ---------- restaurant chain -----------------
+    public static function downloadRestaurantChain($numberOfEmployees, $numberOfLocations, $minSalary, $maxSalary): RestaurantChain {
+        $faker = Factory::create();
+        $DISHES = [
+            "Sushi",
+            "Pizza",
+            "Hamburger",
+            "Pasta",
+            "Fried Chicken",
+            "Ramen",
+            "Taco",
+            "Steak",
+            "Dim Sum",
+            "Falafel",
+            "Paella",
+            "Curry",
+            "Pho",
+            "Biryani",
+            "Fish and Chips",
+            "Tofu Stir Fry",
+            "Gyros",
+            "Peking Duck",
+            "Tamales",
+            "Tom Yum Soup"
+        ];
+
+    // company部分の作成
+    $company = self::downloadCompany($numberOfEmployees);
+
+        return new RestaurantChain(
+            $company->getName(),
+            $company->getFoundingYear(),
+            $company->getDescription(),
+            $company->getWebsite(),
+            $company->getPhone(),
+            $company->getIndustory(),
+            $company->getCeo(),
+            $company->getIsPubliclyTraded(),
+            $company->getCountry(),
+            $company->getFounder(),
+            $company->getTotalEmployees(),
+            rand(1,65536),
+            self::downloadRestaurantLocations($numberOfLocations,$numberOfEmployees, $minSalary, $maxSalary),
+            $faker->randomElement($DISHES),
+            rand(1,10),
+            $faker->company
+        );
+    }
+
+    public static function downloadRestaurantChains($numberOfChains, $numberOfEmployees, $numberOfLocations, $minSalary, $maxSalary): array{
+        $faker = Factory::create();
+        $chains = [];
+
+        for ($i = 0; $i < $numberOfChains; $i++) {
+            $chains[] = self::downloadRestaurantChain($numberOfEmployees, $numberOfLocations, $minSalary, $maxSalary);
+        }
+        
+        return $chains;
+    }
+
+
+
+    // ---------- restaurant location -----------------
+    public static function downloadRestaurantLocation($numberOfEmployees, $minSalary, $maxSalary): RestaurantLocation {
+        $faker = Factory::create();
+
+        return new RestaurantLocation(
+            $faker->company,
+            sprintf("%s %s", $faker->streetName , $faker->secondaryAddress),
+            $faker->city,
+            $faker->state,
+            $faker->postcode,
+            self::downloadEmployees($numberOfEmployees, $minSalary, $maxSalary),
+            $faker->boolean,
+            $faker->boolean
+        );
+    }
+
+    public static function downloadRestaurantLocations($numberOfLocations,$numberOfEmployees, $minSalary, $maxSalary): array{
+        $faker = Factory::create();
+        $locations = [];
+
+        for ($i = 0; $i < $numberOfLocations; $i++) {
+            $locations[] = self::downloadRestaurantLocation($numberOfEmployees, $minSalary, $maxSalary);
+        }
+        
+        return $locations;
+    }
+
+    // ---------- company -----------------
+    public static function downloadCompany($numberOfEmployees): Company {
+        $faker = Factory::create();
+
+        $INDUSTORY = [
+            "Fast Food Restaurants",
+            "Casual Dining Restaurants",
+            "Fine Dining Restaurants",
+            "Cafes & Coffee Shops",
+            "Buffets and All-You-Can-Eat Restaurants",
+            "Food Trucks & Mobile Vending",
+            "Pizzerias",
+            "Ethnic and Specialty Restaurants",
+            "Seafood Restaurants",
+            "Vegetarian and Vegan Restaurants",
+            "Bakeries",
+            "Delis and Sandwich Shops",
+            "Ice Cream and Frozen Yogurt Shops",
+            "Juice Bars and Smoothie Shops",
+            "Pubs and Bars",
+            "Catering Services",
+            "Takeaway and Delivery Services"
+        ];
+
+        return new Company(
+            $faker->name(),
+            rand(1900, 2023),
+            $faker->sentence($nbWords = 6, $variableNbWords = true),
+            $faker->url,
+            $faker->phoneNumber,
+            $faker->randomElement($INDUSTORY),
+            $faker->name(),
+            $faker->boolean,
+            $faker->country,
+            $faker->name(),
+            $numberOfEmployees
+        );
+    }
+    
+    public static function downloadCompanies(int $min, int $max, $numberOfEmployees): array {
+        $faker = Factory::create();
+        $companies = [];
+        $numOfCompanies = $faker->numberBetween($min, $max);
+
+        for ($i = 0; $i < $numOfCompanies; $i++) {
+            $companies[] = self::downloadCompany($numberOfEmployees);
+        }
+
+        return $companies;
+    }
+
+    // ---------- employee -----------------
+
+    /**
+     * ランダムなEmployeeを生成する
+     *
+     * @return Employee
+     */
+    public static function downloadEmployee($minSalary, $maxSalary): Employee {
+        $faker = Factory::create();
+
+        // random jobTitle
+        $JOBTITLE = [
+            "Restaurant Manager",
+            "Executive Chef",
+            "Sous Chef",
+            "Line Cook",
+            "Pastry Chef",
+            "Dishwasher",
+            "Server",
+            "Host/Hostess",
+            "Bartender",
+            "Busser",
+            "Barista",
+            "Sommelier",
+            "Fast Food Cashier",
+            "Drive-Thru Operator",
+            "Food Runner",
+            "Head Waiter",
+            "Kitchen Porter",
+            "Prep Cook",
+            "Maitre d'Hotel",
+            "Baker"
+        ];
+
+        // user
+        // $user = self::user();
+
+        return new Employee(
+            $faker->randomNumber(),
+            $faker->firstName(),
+            $faker->lastName(),
+            $faker->email,
+            $faker->password,
+            $faker->phoneNumber,
+            $faker->address,
+            $faker->dateTimeThisCentury,
+            $faker->dateTimeBetween('-10 years', '+20 years'),
+            $faker->randomElement(['admin', 'user', 'editor']),
+
+            // jobtitle
+            // $faker->jobTitle,
+            $faker->randomElement($JOBTITLE),
+            // salary
+            $faker->randomFloat(2, $minSalary, $maxSalary),
+            // startDate
+            $faker->dateTimeBetween('-30 years', 'now'),
+            // awards
+            self::awards()
+        ) ;
+    }
+
+
+    /**
+     * employeeのawards生成用メソッド
+     * 文字列の配列を返す
+     * 受賞歴なしなら"No awarads received"の文字列が入っただけの配列を返す
+     *
+     * @return array
+     */
+    private static function awards():array {
+        // random awards
+        $AWARDS = [
+            "Best Innovative Company",
+            "Employee's Choice Award",
+            "Leader in Sustainability",
+            "Top 100 Companies to Work For",
+            "Best Customer Service",
+            "Product of the Year",
+            "Best Design Award",
+            "Environmental Leadership",
+            "Top Exporter of the Year",
+            "Fastest Growing Company",
+            "Technology Pioneer Award",
+            "Community Involvement Award",
+            "Best in Diversity & Inclusion",
+            "Corporate Social Responsibility Award",
+            "Excellence in Leadership"
+        ];
+
+        // faker
+        $faker = Factory::create();
+
+        // randomNumber
+        $max = rand(1, 5);
+        $min = 1;
+
+        $awards = [];
+
+        // もし0なら受賞歴なし
+        if ($max == 0 ){ 
+            $awards[] = "No awards received";
+            return $awards;
+        }
+
+        $numOfAwards = $faker->numberBetween($min, $max);
+        for ($i = 0; $i < $numOfAwards; $i++) {
+            $awards[] = $faker->randomElement($AWARDS);
+        }
+
+        return $awards;
+    }
+
+    /**
+     * ランダムなEmployeeの配列を生成する
+     *
+     * @param integer $min
+     * @param integer $max
+     * @return array
+     */
+    public static function downloadEmployees($numberOfEmployees, $minSalary, $maxSalary):array {
+        $faker = Factory::create();
+        $employees = [];
+
+        for ($i = 0; $i < $numberOfEmployees; $i++) {
+            $employees[] = self::downloadEmployee($minSalary, $maxSalary);
+        }
+
+        return $employees;
+    }
+
+
+    // ---------- user ---------------------
+    /**
+     * ランダムなユーザを生成する
+     *
+     * @return User
+     */
+    public static function user(): User {
+        $faker = Factory::create();
+
+        return new User(
+            $faker->randomNumber(),
+            $faker->firstName(),
+            $faker->lastName(),
+            $faker->email,
+            $faker->password,
+            $faker->phoneNumber,
+            $faker->address,
+            $faker->dateTimeThisCentury,
+            $faker->dateTimeBetween('-10 years', '+20 years'),
+            $faker->randomElement(['admin', 'user', 'editor'])
+        );
+    }
+
+    /**
+     * ランダムなユーザーの配列を生成する
+     *
+     * @param integer $min
+     * @param integer $max
+     * @return array
+     */
+    public static function users(int $min, int $max): array {
+        $faker = Factory::create();
+        $users = [];
+        $numOfUsers = $faker->numberBetween($min, $max);
+
+        for ($i = 0; $i < $numOfUsers; $i++) {
+            $users[] = self::user();
+        }
+
+        return $users;
+    }
+}
+
+
+?>
--- app/view/download.php
+++ app/view/download.php
@@ -1,17 +1,14 @@
 <?php
 
 // ----- main.start ------
-// $count = $_POST['count'] ?? 5;
-
-// $count = (int)$count;
 // $users = RandomGenerator::users($count, $count);
 
 // format
 $format = $_POST['format'] ?? 'html';
 
 // numbers
-$numberOfUsers = $_POST['numberOfUsers'] ?? 5;
-$numberOfUsers = (int)$numberOfUsers;
+$numberOfChains = $_POST['numberOfChains'] ?? 5;
+$numberOfChains = (int)$numberOfChains;
 
 $numberOfEmployees = $_POST['numberOfEmployees'] ?? 5;
 $numberOfEmployees = (int)$numberOfEmployees;
@@ -26,12 +23,12 @@ $maxSalary = $_POST['maxSalary'] ?? 10;
 $maxSalary = (int)$maxSalary;
 
 // zipcode
-$minZipCode = $_POST['minZipCode'] ?? 5;
-$maxZipCode = $_POST['maxZipCode'] ?? 10;
+// $minZipCode = $_POST['minZipCode'] ?? 5;
+// $maxZipCode = $_POST['maxZipCode'] ?? 10;
 
 // ----- validations -----
 // is null?
-if (is_null($numberOfUsers)
+if (is_null($numberOfChains)
     || is_null($numberOfEmployees)
     || is_null($numberOfLocations)
     || is_null($minSalary)
@@ -43,8 +40,8 @@ if (is_null($numberOfUsers)
 
 // is in range?
 // count
-if (!is_numeric($numberOfUsers) || $numberOfUsers < 1 || $numberOfUsers > 100) {
-    exit("Invalid number of users. Must be a number between 1 and 100.");
+if (!is_numeric($numberOfChains) || $numberOfChains < 1 || $numberOfChains > 100) {
+    exit("Invalid number of restaurant chains. Must be a number between 1 and 100.");
 }
 // employees
 if (!is_numeric($numberOfEmployees) || $numberOfEmployees < 1 || $numberOfEmployees > 100) {
@@ -69,12 +66,17 @@ if (!in_array($format, $allowdTypes)) {
     exit('Invalied type. Must be one of: ' . implode(', ' , $allowdFormats));
 }
 
+
+// random generate
+// $users = RandomGenerator::users($count, $count);
+$restaurantChains = RandomGenerator::downloadRestaurantChains($numberOfChains, $numberOfEmployees, $numberOfLocations, $minSalary, $maxSalary);
+
 // formatによって処理を分岐
 match ($format) {
-    "markdown" => toMarkdown($users),
-    "json" => toJson($users),
-    "txt" => toText($users),
-    default => toHTML($users) 
+    "markdown" => toMarkdown($restaurantChains),
+    "json" => toJson($restaurantChains),
+    "txt" => toText($restaurantChains),
+    default => toHTML($restaurantChains) 
 };
 // ----- main.end ------
 
@@ -82,35 +84,35 @@ match ($format) {
 // ------ function ------
 
 // markdown形式でダウンロード処理
-function toMarkdown($users) {
+function toMarkdown($restaurantChains) {
         header('Content-Type: text/markdown');
         header('Content-Disposition: attachment; filename="users.md"');
-        foreach ($users as $user) {
+        foreach ($restaurantChains as $user) {
             echo $user->toMarkdown();
         }
 }
 
 // JSON形式でダウンロード処理
-function toJson($users) {
+function toJson($restaurantChains) {
         header('Content-Type: application/json');
         header('Content-Disposition: attachment; filename="users.json"');
-        $userArray = array_map(fn($user) => $user->toArray(), $users);
+        $userArray = array_map(fn($user) => $user->toArray(), $restaurantChains);
         echo json_encode($userArray);
 }
 
 // text形式でダウンロード処理
-function toText($users) {
+function toText($restaurantChains) {
     header('Content-Type: text/plain');
     header('Content-Disposition: attachment; filename="users.txt"');
-    foreach ($users as $user) {
+    foreach ($restaurantChains as $user) {
         echo $user->toString();
     }
 }
 
 // HTML形式で表示
-function toHTML($users) {
+function toHTML($restaurantChains) {
         header('Content-Type: text/html');
-        foreach ($users as $user) {
+        foreach ($restaurantChains as $user) {
             echo $user->toHTML();
         }
 }
